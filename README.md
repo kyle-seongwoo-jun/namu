958fc48eac92c5eadc8f540f11b59afee62a3324
Kyle John <njk230@gmail.com>
2017-11-22 00:41:43 +0900

    Support implicit type and call function

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
 # namu
-Run English Sentences!
+**namu** is a programming language that makes it possible to write code **in English sentences** without knowledge of programming. 
 
-# Exemple
-## Hello world
-```
+![image](docs/parser.png)
+
+# How it works
+name codes are converted to C# codes.
+
+Here is a sample Hello world.
+``` namu
+// namu code
 Console, Write Line "Hello world!".
+
+// after converting
+Console.WriteLine("Hello, World!");
 ```
+
+And converted codes are run on C# Interactive(roslyn).
+
+So you can write and run english sentences without compile.
+
+# Goal
+The primary goal is to make namu a .NET language (without converting to C#), and the ultimate goal is to make namu available in other languages(JVM, python, etc.).
+
+## Example
+``` 
+// on JVM
+System's out, println "Hello, world!".
+
+// python
+print "Hello, world!".
+
+// javascript
+console, log "Hello, world!".
+```
+
+# Progress
+|Features|Progress|
+|--------|--------|
+|Language Specification|In progress|
+|Supporting basic C# syntax|In progress|
+|namu Interactive|Not started|
+|namu basic library|Not started|
+|namu compiler|Not stared|
--- src/Namu/Namu.Parser/Program.cs
+++ src/Namu/Namu.Parser/Program.cs
@@ -8,15 +8,22 @@ namespace Namu.Parser
     {
         static void Main(string[] args)
         {
+            var list = new List<string>();
+
             while (true)
             {
                 Console.Write(">>> ");
 
                 var namu = Console.ReadLine();
 
-                if (namu == "exit") break;
+                if (namu == "history") { list.ForEach(x => Console.WriteLine(x)); Console.WriteLine(); continue; }
+                else if (namu == "clear") { list.Clear(); continue; }
+                else if (namu == "exit") break;
+                else if (namu == "") continue;
 
-                Console.WriteLine(Parser.Parse(namu) + "\n");
+                var result = Parser.Parse(namu);
+                list.Add(result);
+                Console.WriteLine(result + "\n");
             }
         }
     }
@@ -32,24 +39,59 @@ namespace Namu.Parser
             typeKeywordDictionary = new Dictionary<string, string>
             {
                 { "integer", "int" },
+                { "number", "double" },
                 { "text", "string" }
             };
 
             codeParserDictionary = new Dictionary<Regex, Func<Match, string>>
             {
                 { 
-                    // is statement
-                    new Regex("(.+?) (am|are|is) (a|an) (.+?)\\."), (match) =>
+                    // declare variable with am/are/is and a/an operator. 
+                    new Regex(@"([A-Za-z]+) (am|are|is) (a|an) ([A-Za-z]+)\."), (match) =>
                     {
                         var name = match.Groups[1].Value;
                         var type = match.Groups[4].Value;
+                        bool isPrimaryType = false;
 
                         if (typeKeywordDictionary.ContainsKey(type))
                         {
                             type = typeKeywordDictionary[type];
+
+                            isPrimaryType = (type != "string");
                         }
 
-                        return $"var {name} = default({type});";
+                        if (isPrimaryType)
+                        {
+                            return $"var {name} = default({type});";
+                        }
+                        else if (type == "string")
+                        {
+                            return $"var {name} = {type}.Empty;";
+                        }
+                        else
+                        {
+                            return $"var {name} = new {type}();";
+                        }
+                    }
+                },
+                { 
+                    // declare variable with var keyword.
+                    new Regex(@"([A-Za-z]+) (am|are|is) ([A-Za-z]+)\."), (match) =>
+                    {
+                        var name = match.Groups[1].Value;
+                        var value = match.Groups[3].Value;
+                                         
+                        return $"var {name} = {value};";
+                    }
+                },
+                { 
+                    // call method
+                    new Regex(@"([A-Za-z]+) the ([A-Za-z0-9]{1,5}( *, *[A-Za-z0-9]{1,5})*)\."), (match) =>
+                    {
+                        var functionName = match.Groups[1].Value;
+                        var parameters = match.Groups[2].Value;
+
+                        return $"{functionName}({parameters});";
                     }
                 }
             };
